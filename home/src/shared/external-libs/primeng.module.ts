274c067240cc0db3ce289a1a6418487050d46196
Enos Silva <enos.silva@sccon.com.br>
2025-10-08 20:13:53 -0300

    criando parte de contato

--- home/src/shared/external-libs/primeng.module.ts
+++ home/src/shared/external-libs/primeng.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { TooltipModule } from 'primeng/tooltip';
-
+import { InputTextModule } from 'primeng/inputtext';
 @NgModule({
   declarations: [],
   imports: [
     CarouselModule,
-    TooltipModule
+    TooltipModule,
+    InputTextModule
   ],
   exports: [
     CarouselModule,
-    TooltipModule
+    TooltipModule,
+    InputTextModule
   ]
 })
 export class PrimengModule { }
--- home/src/shared/shared.module.ts
+++ home/src/shared/shared.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimengModule } from './external-libs/primeng.module';
+import { MaterialModule } from './external-libs/material.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    PrimengModule
+    CommonModule
   ],
   exports: [
-    PrimengModule
+    PrimengModule,
+    MaterialModule
   ]
 })
 export class SharedModule { }
